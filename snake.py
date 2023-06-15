b9c65b4fcab6c51b2d0cf4ea06f8798360eea3d4
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2023-06-15 21:13:35 +0545

    window screen created

---
+++ snake.py
@@ -0,0 +1,19 @@
+# import pygame
+# pygame.init()
+# display=pygame.display.set_mode((800,600))
+# pygame.display.update()
+# pygame.display.set_caption("Snake Game")
+# game_over=False
+# while not game_over:
+#     for event in pygame.event.get():
+#         if event.type==pygame.QUIT:
+#             game_over=True
+      
+# pygame.quit()
+# quit()
+
+import pygame 
+width=500
+height= 400
+pygame.init()
+screen=pygame.display.set_mode((width,height))
