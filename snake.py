3cca0c76d73cdafcba636ef7f6586582545eff1c
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2023-06-15 22:26:57 +0545

    converting png

--- snake.py
+++ snake.py
@@ -11,11 +11,11 @@ clock = pygame.time.Clock()
 #creating font
 test_font=pygame.font.Font("font\Pixeltype.ttf",50)
 #creating surface
-sky_surface=pygame.image.load("graphics/Sky.png")
-ground_surface=pygame.image.load("graphics/ground.png")
+sky_surface=pygame.image.load("graphics/Sky.png").convert()
+ground_surface=pygame.image.load("graphics/ground.png").convert()
 text_surface=test_font.render("My game",False,"Black")
 #snail
-snail_surface=pygame.image.load("graphics\snail\snail1.png")
+snail_surface=pygame.image.load("graphics\snail\snail1.png").convert()
 snail_x_pos=700
 while True:
     for event in pygame.event.get():
@@ -27,8 +27,11 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(330,50))
-    snail_x_pos-=1
+    snail_x_pos-=6
+    if snail_x_pos<-50:
+        snail_x_pos=800
     screen.blit(snail_surface,(snail_x_pos,268))
+    
 
     pygame.display.update()
     clock.tick(60) # sets max frame to 60fps
