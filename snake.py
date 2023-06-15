5870891b92adf66c67e787bce277bc82be3626d5
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2023-06-15 22:10:49 +0545

    snail added and now moves

--- snake.py
+++ snake.py
@@ -7,22 +7,28 @@ pygame.init()
 screen=pygame.display.set_mode((width,height))
 pygame.display.set_caption("Snake game")
 clock = pygame.time.Clock()
+
 #creating font
-test_font=pygame.font.Font(None,50)
+test_font=pygame.font.Font("font\Pixeltype.ttf",50)
 #creating surface
 sky_surface=pygame.image.load("graphics/Sky.png")
 ground_surface=pygame.image.load("graphics/ground.png")
-text_surface=test_font.render("My game",False,"Green")
-
+text_surface=test_font.render("My game",False,"Black")
+#snail
+snail_surface=pygame.image.load("graphics\snail\snail1.png")
+snail_x_pos=700
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
+
     #showing surface to the screen using coordinate        
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(325,50))
+    screen.blit(text_surface,(330,50))
+    snail_x_pos-=1
+    screen.blit(snail_surface,(snail_x_pos,268))
 
     pygame.display.update()
     clock.tick(60) # sets max frame to 60fps
