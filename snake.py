65fef876bf452308769600713b953ae3d590053d
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2023-06-15 22:01:24 +0545

    font created and converted to image and displayed over surface

--- snake.py
+++ snake.py
@@ -2,20 +2,27 @@
 import pygame
 from sys import exit
 width=800
-height= 600
+height= 400
 pygame.init()
 screen=pygame.display.set_mode((width,height))
 pygame.display.set_caption("Snake game")
 clock = pygame.time.Clock()
-test_surface= pygame.Surface((100,200))
-test_surface.fill('red')
+#creating font
+test_font=pygame.font.Font(None,50)
+#creating surface
+sky_surface=pygame.image.load("graphics/Sky.png")
+ground_surface=pygame.image.load("graphics/ground.png")
+text_surface=test_font.render("My game",False,"Green")
 
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
-    screen.blit(test_surface,(200,100))
+    #showing surface to the screen using coordinate        
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(325,50))
 
     pygame.display.update()
     clock.tick(60) # sets max frame to 60fps
