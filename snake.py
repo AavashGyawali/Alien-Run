5c62d73bffeb8ff1add4a04bbb62a1f8aa165e27
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2023-06-15 21:23:12 +0545

    title rename

--- snake.py
+++ snake.py
@@ -5,7 +5,7 @@ width=500
 height= 400
 pygame.init()
 screen=pygame.display.set_mode((width,height))
-
+pygame.display.set_caption("Snake game")
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
