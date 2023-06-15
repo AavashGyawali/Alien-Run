7346663d7260042ca47ec277ff8e143468c974d5
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2023-06-15 21:21:45 +0545

    constant screen

--- snake.py
+++ snake.py
@@ -1,19 +1,16 @@
-# import pygame
-# pygame.init()
-# display=pygame.display.set_mode((800,600))
-# pygame.display.update()
-# pygame.display.set_caption("Snake Game")
-# game_over=False
-# while not game_over:
-#     for event in pygame.event.get():
-#         if event.type==pygame.QUIT:
-#             game_over=True
-      
-# pygame.quit()
-# quit()
 
-import pygame 
+import pygame
+from sys import exit
 width=500
 height= 400
 pygame.init()
 screen=pygame.display.set_mode((width,height))
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
+    
