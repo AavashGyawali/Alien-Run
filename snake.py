28b09d5b658a5b3f329abbaed3ed03268a8539dd
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2023-06-15 21:39:11 +0545

    test surface

--- snake.py
+++ snake.py
@@ -1,19 +1,22 @@
 
 import pygame
 from sys import exit
-width=500
-height= 400
+width=800
+height= 600
 pygame.init()
 screen=pygame.display.set_mode((width,height))
 pygame.display.set_caption("Snake game")
 clock = pygame.time.Clock()
+test_surface= pygame.Surface((100,200))
+test_surface.fill('red')
 
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
+    screen.blit(test_surface,(200,100))
 
     pygame.display.update()
-    clock.tick(60)# sets max frame to 60fps
+    clock.tick(60) # sets max frame to 60fps
     
