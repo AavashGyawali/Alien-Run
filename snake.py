366b02ec22eee9370edaaebe2b9112931014a8a0
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2023-06-15 21:26:54 +0545

    set max fps to 60

--- snake.py
+++ snake.py
@@ -6,6 +6,8 @@ height= 400
 pygame.init()
 screen=pygame.display.set_mode((width,height))
 pygame.display.set_caption("Snake game")
+clock = pygame.time.Clock()
+
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -13,4 +15,5 @@ while True:
             exit()
 
     pygame.display.update()
+    clock.tick(60)# sets max frame to 60fps
     
