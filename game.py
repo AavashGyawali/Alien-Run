4d513813922ae63ef675903dfee79c470914cd16
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2023-06-17 22:35:13 +0545

    starting and ending page created

--- game.py
+++ game.py
@@ -1,26 +1,43 @@
 
 import pygame
 from sys import exit
+
+
+def display_score():
+    current_time=int(pygame.time.get_ticks()/120)-start_time
+    return current_time
+    
+
+
 width=800
 height= 400
 pygame.init()
 screen=pygame.display.set_mode((width,height))
-pygame.display.set_caption("Snake game")
+pygame.display.set_caption("Alien Run")
 clock = pygame.time.Clock()
 
 #creating font
 test_font=pygame.font.Font("font\Pixeltype.ttf",50)
 
 
-game_active=True
+game_active=False
+start_time=0
+score =0
 
 #creating surface
 sky_surface=pygame.image.load("graphics/Sky.png").convert()
 ground_surface=pygame.image.load("graphics/ground.png").convert()
 
-#Score
-score_surface=test_font.render(" My game",False,(64,64,64))
-score_rec=score_surface.get_rect(center=(400,50))
+#Title
+title_surface=test_font.render("Alien Run",False,(64,64,64))
+title_rect=title_surface.get_rect(center=(400,25))
+
+
+
+
+#Result
+result_surface=test_font.render(f"Your Score is ",False,(64,64,64))
+result_rec=result_surface.get_rect(center=(400,25))
 
 #snail surface
 snail_surface=pygame.image.load("graphics\snail\snail1.png").convert_alpha()
@@ -31,11 +48,26 @@ player_surface=pygame.image.load("graphics/player/player_walk_1.png").convert_al
 player_rec=player_surface.get_rect(bottomleft=(80,300))
 player_gravity=0
 
+
+#intro screen
+player_stand_surface=pygame.image.load("graphics\Player\player_stand.png").convert_alpha()
+player_stand_surface=pygame.transform.rotozoom(player_stand_surface,0,2)
+player_stand_rect=player_stand_surface.get_rect(center=(400,200))
+
+#intro Text
+intro1_surface=test_font.render("Alien Run",False,(64,64,64))
+intro1_rect=intro1_surface.get_rect(center=(400,25))
+
+intro2_surface=test_font.render("Press  Space  to  start",False,(64,64,64))
+intro2_rect=intro2_surface.get_rect(center=(400,320))
+
+
+
 #Game Over
 game_over_surface=test_font.render("Game Over",False,(64,64,64))
 game_over_rec=game_over_surface.get_rect(center=(400,150))
 
-to_restart_surface=test_font.render("To Restart press Space ",False,(64,64,64))
+to_restart_surface=test_font.render("Press  Space  to  Restart",False,(64,64,64))
 to_restart_rec=to_restart_surface.get_rect(center=(400,200))
 
 
@@ -50,7 +82,7 @@ while True:
 #if space key is pressed player will jump
             if event.type == pygame.KEYDOWN:
                 if event.key==pygame.K_SPACE and player_rec.bottom >=300:
-                        player_gravity=-20
+                        player_gravity=-22
                         # player_gravity+=1
                 
         
@@ -59,12 +91,13 @@ while True:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if pygame.mouse.get_pressed():
                     if player_rec.collidepoint(event.pos):#instead of event.pos you can write pygame.mouse.get_pos()
-                        player_gravity=-20
+                        player_gravity=-22
 
         else:
             if event.type == pygame.KEYDOWN and event.key==pygame.K_SPACE:
                 game_active=True
                 snail_rec.left=800
+                start_time=int(pygame.time.get_ticks()/120)
 
 
 
@@ -74,15 +107,20 @@ while True:
         #showing surface to the screen using coordinate        
         screen.blit(sky_surface,(0,0))
         screen.blit(ground_surface,(0,300))
-        pygame.draw.rect(screen,"#c0e8ec",score_rec,border_radius=2)
-        pygame.draw.rect(screen,"black",score_rec,2,border_radius=2)
+        screen.blit(title_surface,title_rect)
+        score=display_score()
+
+
+        #Score Text
+        #Score
+        score_surface=test_font.render(f'Score: {score}',False,(64,64,64))
+        score_rec=score_surface.get_rect(topleft=(10,10))
         screen.blit(score_surface,score_rec)
-        # pygame.draw.ellipse(screen,"black",pygame.Rect(50,200,50,100))
+
+
 
         #for snail   
         screen.blit(snail_surface,snail_rec)
-
-        
         snail_rec.left-=4
         if snail_rec.right<0:snail_rec.left=800
 
@@ -101,16 +139,21 @@ while True:
 
 #After Collision
     else:
-        # screen.fill('yellow')
-        # pygame.draw.rect(screen,"#c0e8ec",game_over_rec,border_radius=2)
-        # pygame.draw.rect(screen,"black",score_rec,2,border_radius=2)
-        screen.blit(game_over_surface,game_over_rec)
-        screen.blit(to_restart_surface,to_restart_rec)
+            if score ==0:
+                screen.fill('white')
+                screen.blit(player_stand_surface,player_stand_rect)
+                screen.blit(intro1_surface,intro1_rect)
+                screen.blit(intro2_surface,intro2_rect)
+
+
+            else:
+                screen.blit(game_over_surface,game_over_rec)
+                screen.blit(to_restart_surface,to_restart_rec)
+
+
         
-
-
-            
-         
+        
+        
 
     pygame.display.update()
     clock.tick(60) # sets max frame to 60fps
